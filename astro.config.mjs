802771958c3f5af2071e1c8c76ea06944c5ebddf
Guilherme Nicolini <guilherme.nicolini@ingaia.com.br>
2023-08-14 15:11:56 -0300

    feat: add shiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx"
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   site: 'https://guilhermenicolini.com.br',
   markdown: {
-    syntaxHighlight: false
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
   }
 });
